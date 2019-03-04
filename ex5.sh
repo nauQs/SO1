71f400ab7e819a79b785e55fa7d55f99e28e017c
arnauquindos <aquindsa10@alumnes.ub.edu>
2019-03-04 18:58:14 +0100

    4 i 5 fets (falta arreglar 4)

--- ex5.sh
+++ ex5.sh
@@ -6,17 +6,26 @@ then
    exit 1
 fi
 
-dir=$1
+directoriactual=$(pwd)
+dir=$directoriactual/$1
 
-fitxers=$(ls $dir)
 
-for directori in $fitxers
+echo $dir
+if [ ! -d $dir ]
+then
+	echo sortim
+	exit 2
+fi
+
+for subdir in $(ls $dir)
 do
-   adr=$dir/$fitxer
-   if [ -d $adr ]
-   then 
-      
-      
+   adr=$dir/$subdir
+   if [ -f $adr ]
+   then
+	echo $adr
+   else	
+	echo crida recursiva $adr
+	./$0 $adr
    fi
 done
 exit 0
