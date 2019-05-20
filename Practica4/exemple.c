9a2c4ca24ed1d4c58a8e7522c86c7ea86fe3894e
nauQs <naukin2@gmail.com>
2019-05-20 16:28:13 +0200

    exemple.c updated

--- Practica4/exemple.c
+++ Practica4/exemple.c
@@ -4,13 +4,15 @@
 int main()
 {
   int i;
-  int *p, *q;
+  int *p, *q, *a, *b, *c, *d;
 
   p = malloc(10 * sizeof(int));
   q = malloc(10 * sizeof(int));
   p = malloc(102 * sizeof(int));
+  free(p);
   q = malloc(104 * sizeof(int));
-  //p = calloc(10 , sizeof(int));
+  free(q);
+  p = calloc(10 , sizeof(int));
 
   for(i = 0; i < 10; i++){
     p[i] = i;
@@ -19,14 +21,23 @@ int main()
   for(i = 0; i < 10; i++){
     printf("%d\n", p[i]);
   }
-    
-  //q = realloc(p , sizeof(int)*20);
+
+  a = calloc(10 , sizeof(int));
+  b = calloc(3 , sizeof(int));
+  c = calloc(2 , sizeof(int));
+  d = calloc(5 , sizeof(int));
+  free(a);
+  free(b);
+  free(c);
+  free(d);
+  q = realloc(p , sizeof(int)*20);
   
   for(i = 0; i < 20; i++){
     printf("%d\n", q[i]);
   }
     
   free(p);
+  free(q);
 
   return 0;
 }
